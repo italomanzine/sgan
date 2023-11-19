a9282aa90a7344b83c48d6e6977921eb57f07da3
Ítalo Manzine <italomanzine@gmail.com>
2023-11-18 22:06:08 -0300

    Add sidebar design

--- sgan/core/static/js/sidebar.js
+++ sgan/core/static/js/sidebar.js
@@ -1,20 +1,35 @@
 // static/js/sidebar.js
-// Quando o DOM estiver carregado
-document.addEventListener('DOMContentLoaded', function() {
-    var sidebar = document.querySelector('.sidebar');
-    var toggleButton = document.querySelector('.sidebar-toggle');
 
-    toggleButton.addEventListener('click', function() {
-        // Adiciona ou remove a classe 'active' do sidebar
-        sidebar.classList.toggle('active');
-        // Altera o ícone do botão de acordo com o estado do sidebar
-        if (sidebar.classList.contains('active')) {
-            toggleButton.textContent = ''; // Limpa o texto do botão
-            toggleButton.classList.add('active'); // Adiciona classe 'active' para o botão
-        } else {
-            toggleButton.textContent = '☰ Menu'; // Texto original do botão
-            toggleButton.classList.remove('active'); // Remove classe 'active' do botão
-        }
-    });
-});
-  
+// Função para alternar o estado do sidebar
+function setSidebarState(isMinimized) {
+    var sidebar = document.querySelector(".sidebar");
+    var centerContent = document.querySelector(".center-content");
+    var toggleIcon = document.querySelector(".sidebar-toggle i");
+
+    sidebar.style.width = isMinimized ? "60px" : "250px";
+    centerContent.style.marginLeft = isMinimized ? "60px" : "250px";
+    toggleIcon.classList.toggle("fa-bars", isMinimized);
+    toggleIcon.classList.toggle("fa-arrow-left", !isMinimized);
+    sidebar.classList.toggle("minimized", isMinimized);
+    sidebar.classList.toggle("active", !isMinimized);
+}
+
+// Função para alternar o estado do sidebar
+function toggleSidebar() {
+    var sidebar = document.querySelector(".sidebar");
+    setSidebarState(!sidebar.classList.contains("minimized"));
+}
+
+// Função para inicializar o sidebar
+function initializeSidebar() {
+    setSidebarState(window.innerWidth < 768);
+}
+
+// Evento de clique do botão de alternância do sidebar
+document.querySelector(".sidebar-toggle").addEventListener("click", toggleSidebar);
+
+// Evento para inicializar o sidebar no carregamento da página
+window.addEventListener("load", initializeSidebar);
+
+// Evento para ajustar o sidebar ao redimensionar a janela
+window.addEventListener("resize", initializeSidebar);
